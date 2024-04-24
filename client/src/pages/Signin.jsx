670c636fb72f434f6f924d0f3984395b2cc66114
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-23 21:38:16 -0300

    Completed Signin functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import { Alert, Button, Label, TextInput, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);  
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  //Sign up function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);      
+      setErrorMessage("");
+      if (!formData.email || !formData.password) {
+        setErrorMessage("Please fill all fields");
+      } else {
+        const res = await fetch("/api/auth/signin", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          setErrorMessage(data.message);
+          return false;
+        }
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      Signin
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/*Left side */}
+        <div className="flex-1">
+          <Link to="/" className="text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Girish's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p>
+        </div>
+        {/*Right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>            
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+                value={formData.email}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+                value={formData.password}
+              />
+            </div>
+            <Button
+              disabled={loading}
+              gradientDuoTone="purpleToPink"
+              type="submit"
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In "
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-4">
+            <span>Don't have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1"></div>
+        <div className="flex-1">
+          {errorMessage && (
+            <Alert className="mt-4" color="failure">
+              {errorMessage}
+            </Alert>
+          )}          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
