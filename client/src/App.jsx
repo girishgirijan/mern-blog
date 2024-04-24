0cb228a81bf71f4555d771bf2428b7f3ab040ac9
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-24 17:49:13 -0300

    Made Dashboard component private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,17 +7,20 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Project";
 import Header from "./components/Header";
 import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterCom />
