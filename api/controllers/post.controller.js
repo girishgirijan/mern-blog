c01a454124b56393cb2423d42da51e5e4a6cf68f
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-27 17:08:10 -0300

    Completed getposts API

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,7 +8,10 @@ export const create = async (req, res, next) => {
     return next(errorHandler(400, "Please fill Title and Content"));
 
   const existingTitle = await Post.findOne({ title: req.body.title });
-  if (existingTitle) return next(errorHandler(400, "Title is alreay in use. Please try different"));
+  if (existingTitle)
+    return next(
+      errorHandler(400, "Title is alreay in use. Please try different")
+    );
 
   const slug = req.body.title
     .split(" ")
@@ -24,6 +27,49 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Get all posts
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
   } catch (error) {
     next(error);
   }
