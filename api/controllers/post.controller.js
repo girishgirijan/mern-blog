68e3d49726a7c58622187ac03ef7746e3ffeec2e
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-27 12:36:21 -0300

    Implemented create post API route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,30 @@
+import { errorHandler } from "../utils/error.js";
+import Post from "../models/post.model.js";
+
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not allowded to create post"));
+  if (!req.body.title || !req.body.content)
+    return next(errorHandler(400, "Please fill all fields"));
+
+  const existingTitle = await Post.findOne({ title: req.body.title });
+  if (existingTitle) return next(errorHandler(400, "Title is alreay in use. Please try different"));
+
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
@@ -19,7 +20,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Sever is running on port 3000");
@@ -31,6 +32,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
