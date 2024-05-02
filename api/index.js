49b8fa66a5cdcdb0c7d9096956456afdb7187486
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-02 17:51:58 -0300

    Deploy update

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
-import path from 'path'
+import path from "path";
 
 dotenv.config();
 
@@ -20,8 +20,15 @@ mongoose
     console.log("Error:", error);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // '/client/dist/index.html'
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -38,7 +45,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")))
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
